d4d346071463cde5b8a48c3534157b96b2a43df3
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-11-20 17:54:28 +0800

    refact:delete unused comment

--- src/main/java/WordFrequency.java
+++ src/main/java/WordFrequency.java
@@ -2,7 +2,7 @@ public class WordFrequency {
     private String word;
     private int count;
 
-    public WordFrequency(String word, int count){//parameter rename
+    public WordFrequency(String word, int count){
         this.word =word;
         this.count =count;
     }
