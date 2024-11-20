1d187c78acc103e97cf4f387e6fd02448421d5e4
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-11-20 14:14:11 +0800

    refact:delete useless spaces

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -2,35 +2,26 @@ import java.util.*;
 
 public class WordFrequencyGame {
     public String getResult(String inputStr) {//rename
-
-
         if (inputStr.split("\\s+").length == 1) {//constant
             return inputStr + " 1";
         } else {//unuse else
-
             try {//do to many
-
                 //split the input string with 1 to n pieces of spaces
                 String[] arr = inputStr.split("\\s+");
-
                 List<Input> inputList = new ArrayList<>();
                 for (String s : arr) {//stream
                     Input input = new Input(s, 1);
                     inputList.add(input);
                 }
-
                 //get the map for the next step of sizing the same word
                 Map<String, List<Input>> map = getListMap(inputList);//param rename
-
                 List<Input> list = new ArrayList<>();
                 for (Map.Entry<String, List<Input>> entry : map.entrySet()) {//stream
                     Input input = new Input(entry.getKey(), entry.getValue().size());
                     list.add(input);//unneed temp value
                 }
                 inputList = list;
-
                 inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());//param rename
-
                 StringJoiner joiner = new StringJoiner("\n");
                 for (Input w : inputList) {//stream;for loop ->function
                     String s = w.getValue() + " " + w.getWordCount();
@@ -38,13 +29,10 @@ public class WordFrequencyGame {
                 }
                 return joiner.toString();
             } catch (Exception e) {
-
-
                 return "Calculate Error";
             }
         }
     }
-
 
     private Map<String, List<Input>> getListMap(List<Input> inputList) {
         Map<String, List<Input>> map = new HashMap<>();
@@ -58,8 +46,6 @@ public class WordFrequencyGame {
                 map.get(input.getValue()).add(input);
             }
         }
-
-
         return map;
     }
 
